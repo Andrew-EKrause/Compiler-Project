9c2244bfadf816234ce5f548da76beb3cf64ae08
Andrew Krause <aek.krause@gmail.com>
2022-01-28 19:10:14 -0600

    Wrote functions for symbol table. Currently have 8/10 written.

--- Compiler/SymTab.c
+++ Compiler/SymTab.c
@@ -23,6 +23,7 @@
  * attributes of the table, and setting all values to NULL
  * at the start.
  */
+// --> SEE IF THERE IS A MORE SIMPLE WAY TO DO THIS!!!
 SymTab *createSymTab(int size) {
 
     // Create space in memory for the symbol table.
@@ -32,7 +33,7 @@ SymTab *createSymTab(int size) {
     // include the table size and the table contents. 
     // Create space in memory for the symbol table contents.
     symbolTable->size = size;
-    symbolTable->contents = malloc(sizeof(SymEntry*) * symbolTable->size);
+    symbolTable->contents = malloc(sizeof(SymEntry*) * symbolTable->size); // MAY NEED TO ADD +1 FOR CURRENT
 
     // Create a for loop to set all of the entries in the symbol
     // table to NULL. The entries are encompassed in the 
@@ -56,6 +57,7 @@ SymTab *createSymTab(int size) {
  * the various attributes of the table to NULL as well
  * as by freeing all of the data attributes.
  */
+// --> SEE IF THERE IS A MORE SIMPLE WAY TO DO THIS!!!
 void destroySymTab(SymTab *table) {
 
     // Create temporary variables to help free the 
@@ -116,29 +118,176 @@ void destroySymTab(SymTab *table) {
 }
 
 /**
- * DESCRIPTION...
+ * The function inserts a new name into the symbol
+ * table. If name is not in the symbol table, a copy 
+ * of name is added to the symbol table with a NULL 
+ * attribute, set current to reference the new (name, attribute) 
+ * pair and return 1 if name is in the symbol table, 
+ * set current to reference the (name, attribute) pair 
+ * and return 0.
  */
-// --> DELETE LATER --> NEED TO COMPLETE THE findName FUNCTION FIRST!!!
+// --> COMPLETE LATER!!!
 int enterName(SymTab *table, char *name) {
 
+    // If the name is not already in the symbol table,
+    // make a copy of the name and add it to the
+    // symbol table.
+    if(findName(table, name) == 0) {
+
+    } else {
+
+        // If the name is already in the symbol table,
+        // set current to reference the name attribute
+        // pair and return 0.
+        return 0;
+    }
 }
-/*
-    if name is not in the symbol table, a copy of name is added to the symbol table
-    with a NULL attribute, set current to reference the new (name, attribute) pair
-    and return 1
-    if name is in the symbol table, set current to reference the (name, attribute)
-    pair and return 0
-*/
 
 /**
- * DESCRIPTION...
+ * If the name is in the symbol table, set current
+ * to reference the (name, attribute) pair and return
+ * 1. Otherwise, do not change current and return 0.
  */
-// --> DELETE LATER --> NEED TO COMPLETE THE findName FUNCTION FIRST!!!
 int findName(SymTab *table, char *name) {
+
+    // Create temporary variables to help
+    // determine if the name is in the table.
+    SymEntry *currentSlot = NULL;
+    SymEntry *nextEntryPoint = NULL;
+    SymEntry *specificEntry = NULL;
+
+    // Create a for loop to visit each entry in the
+    // array of linked lists and determine if the 
+    // name is in the symbol table.
+    int i;
+    for(i = 0; i < table->size; i++) {
+
+        // Move through each slot in the symbol table.
+        currentSlot = table->contents[i]; 
+        specificEntry = currentSlot; // --> SEE IF THIS VARIABLE NAME IS NECESSARY!!!
+
+        // In any given row of the symbol, traverse  
+        // until last entry is reached.
+        while(specificEntry != NULL) {
+
+            // If the name in the symbol table is found,
+            // set current to reference the (name, attribute) 
+            // pair and return 1.
+            if(strcmp(specificEntry->name, name) == 0) {
+
+                table->current = specificEntry;
+                return 1;
+            }
+
+            // Move to the next entry in any 
+            // given row of the symbole table.
+            nextEntryPoint = specificEntry->next;
+            specificEntry = nextEntryPoint;
+        }
+    }
+
+    // If the name is not in the symbol table, 
+    // do not change anything and return 0.
+    return 0;
+}
+
+/**
+ * The function checks if the current attribute of 
+ * the Symbol Table references a name. If current
+ * references a name, return 1. Otherwise, return 0.
+ */
+int hasCurrent(SymTab *table) {
+
+    // If current references a name, indicating 
+    // that it is not NULL, return 1. Otherwise,
+    // return 0.
+    if(table->current != NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * The function sets the current attribute
+ * in the symbol table to the specified
+ * parameter attribute.
+ */
+void setCurrentAttr(SymTab *table, void * attr) {
+
+    // PRE: hashCurrent() == 1
+    // change the attribute value of the 
+    // current (name, attribute) pair to attr
+    table->current->attribute = attr;
+}
+
+/**
+ * The function gets the current attribute
+ * in the symbol table. 
+ */
+void * getCurrentAttr(SymTab *table) {
+
+    // PRE: hasCurrent() == 1
+    // return the attribute in the 
+    // current (name, attribute) pair
+    table->current->attribute;
+}
+
+/**
+ * The function gets the name of the
+ * current (name, attribute) pair in 
+ * the symbol table. The value is 
+ * returned from the function,
+ */
+char * getCurrentName(SymTab *table) {
+
+    // PRE: hasCurrent() == 1
+    // return the name in the 
+    // current (name, attribute) pair
+    return table->current->name;
+}
+
+/**
+ * The function sets current to the "first" (name, attribute)
+ * pair in the symbol table and return 1. Otherwise, if the
+ * table is empty, return 0.
+ */
+int startIterator(SymTab *table) {
+
+    // Create variables to help search through
+    // the symbol table and set current.
+    int i;
+    SymEntry *currentSlot = NULL;
+
+    // Loop through all the slots in the symbol table
+    // until you find the "first" (name, attribute) pair.
+    for(i = 0; i < table->size; i++) {
+
+        // Set the temp variable equal to
+        // each slot in the symbol table.
+        currentSlot = table->contents[i];
+
+        // If the slot is not NULL, then set values.
+        if(currentSlot != NULL) {
+
+            // Set current equal to the "first" name
+            // attribute pair in the table and return 1.
+            table->current = currentSlot;
+            return 1;
+        }
+    }
+
+    // If the table is empty, return 0.
+    return 0;
+}
+
+
+
+
+int nextEntry(SymTab *table) {
 
 }
 /*
-    if name is in the symbol table, set current to reference the (name, attribute)
-    pair and return 1
-    otherwise do not change current and return 0
+    if all (name, attribute) pairs have been visited since the last call to
+    startIterator, return 0
+    otherwise set current to the "next" (name, attribute) pair and return 1
 */
